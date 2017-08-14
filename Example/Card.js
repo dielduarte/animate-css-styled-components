e4774a637f51fadf753e34dcf5d23b9070f7a4d6
dielduarte <magdiel.kesller@gmail.com>
2017-08-13 22:50:08 -0300

    fix styled syntax

--- Example/Card.js
+++ Example/Card.js
@@ -6,10 +6,10 @@ const Card = styled.div`
   padding: 10px;
   background: #FFF;
   box-shadow: 0 2px 8px rgba(14,20,26,0.2);
-  ${props => props.rounded ? 'border-radius: 50%;': ''}
-  width: ${props => props.width ? props.width : 'auto'}
-  height: ${props => props.height ? props.height : 'auto'}
-  ${flexCenter}
+  ${props => props.rounded ? 'border-radius: 50%;': ''};
+  width: ${props => props.width ? props.width : 'auto'};
+  height: ${props => props.height ? props.height : 'auto'};
+  ${flexCenter};
 
   &:hover {
     cursor: pointer;
